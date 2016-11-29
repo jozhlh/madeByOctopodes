3b36dbddcb0767297842af61280b63dec73496c1
rizlaking <josh_hale@btinternet.com>
2016-11-29 14:25:50 +0000

    Added Splash Screen
    Added splash screen for when game is loaded

---
+++ Flying_Mechanic_Lanes/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+	public void LoadPrototypeLevel()
+	{
+		SceneManager.LoadScene("prototype_0");
+	}
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("menu");
+	}
+}
