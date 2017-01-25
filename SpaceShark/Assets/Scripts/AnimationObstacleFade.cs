a0c110e7523ee989d6e52f1b6c077908779dbe21
Josh Hale <josh_8d@hotmail.co.uk>
2017-01-25 17:39:32 +0000

    Documented AnimationObstacleFade

--- SpaceShark/Assets/Scripts/AnimationObstacleFade.cs
+++ SpaceShark/Assets/Scripts/AnimationObstacleFade.cs
@@ -3,49 +3,51 @@ using System.Collections;
 
 public class AnimationObstacleFade : MonoBehaviour {
 
-//  [SerializeField]
-  //  private GameObject player = null;
-
-    private Obstacle thisObstacle = null;
-
-    private Renderer[] attachedRenderers = null;
-
-    private Material mat = null;
-
+    [Header("Fade Distance Settings")]
     [SerializeField]
-    private Material solidMaterial = null;
-
+    // The lowest level the alpha value will lerp to
+    private float lowestTransparency = 0.2f;     
     [SerializeField]
-    private Material fadeMaterial = null;
-
-    [SerializeField]
-    private Color targetColour = Color.magenta;
-
-    public float transparency = 1.0f;
-    [SerializeField]
-    private float lowestTransparency = 0.2f;
-    [SerializeField]
+    // The distance from the player an obstacle in the top lane will begin to fade out
     private float playerDistanceTop = 10.0f;
     [SerializeField]
+    // The distance from the player an obstacle in the middle lane will begin to fade out
     private float playerDistanceMid = 5.0f;
 
-	private int numberOfRenderers;
+    [Header("Shader Settings")]
+    [SerializeField]
+    // A material with a diffuse shader
+    private Material solidMaterial = null;
+    [SerializeField]
+    // A material with the fade shader
+    private Material fadeMaterial = null;
+    [SerializeField]
+    // Used for holding the correct transparency value
+    private Color targetColour = Color.magenta;
 
+    // Is the player withing the range specified
     private bool playerInRange = false;
+    // The current alpha value
+    private float transparency = 1.0f;
+    // How many renderers are attached to children
+    private int numberOfRenderers;
+    // The obstacle class attached to this object
+    private Obstacle thisObstacle = null;
+    // The renderers attached to all children
+    private Renderer[] attachedRenderers = null;
+    // Used for updating the rendering with the correct shader
+    private Material mat = null;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        // Get references to the obstacle object and all renderers in children 
         thisObstacle = gameObject.GetComponent<Obstacle>();
-
-        //attachedRenderer = gameObject.GetComponent<Renderer>();
-        // attachedRenderer.sharedMaterial.color = targetColour;
-       // gameObject.GetComponent<Renderer>().material = solidMaterial;
-       // mat = gameObject.GetComponent<Renderer>().material;
-        //mat.SetColor("_Color", targetColour);
-
 		numberOfRenderers = GetComponentsInChildren<Renderer>().Length;
 		attachedRenderers = new Renderer[numberOfRenderers];
 		attachedRenderers = GetComponentsInChildren<Renderer>();
+
+        // Apply the opaque mobile shader to all objects in children
 		for (int i = 0; i < numberOfRenderers; i++)
 		{
 			attachedRenderers[i].material = solidMaterial;
@@ -55,11 +57,16 @@ public class AnimationObstacleFade : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        // Get current position in level
         float playerZ = Ship_Movement.shipPosition.z;
 
+        // If the obstacle intersects with the top lane at any point, check if the player is in range
         if (((int)thisObstacle.location < 5) | (((int)thisObstacle.location > 6) & ((int)thisObstacle.location < 10))) // NW_R, NW_D, N, NE_D, NE_L, SE_U, S, SW_U
         {
+            // If the player is in range of this obstacle, get the current fade value, otherwise set the fade value to 1.0f
+            //TODO: Linearly Interpolate this value rather than clamping to give a smoother fade
             if ((thisObstacle.GetzPosition() - playerZ) < playerDistanceTop)
             {
                 playerInRange = true;
@@ -75,8 +82,11 @@ public class AnimationObstacleFade : MonoBehaviour {
                 transparency = 1.0f;
             }
         }
+        // If the obstacle is just in the middle lane, check if the player is nin range
         else if ((thisObstacle.location == LaneManager.ObstacleLocation.E) | (thisObstacle.location == LaneManager.ObstacleLocation.W)) // E, W
         {
+            // If the player is in range of this obstacle, get the current fade value, otherwise set the fade value to 1.0f
+            //TODO: Linearly Interpolate this value rather than clamping to give a smoother fade
             if ((thisObstacle.GetzPosition() - playerZ) < playerDistanceMid)
             {
                 playerInRange = true;
@@ -86,26 +96,21 @@ public class AnimationObstacleFade : MonoBehaviour {
                     transparency = lowestTransparency;
                 }
             }
-            else // SE_L, SW_R
+            else 
             {
                 playerInRange = false;
                 transparency = 1.0f;
             }
         }
-        else
+        else // SE_L, SW_R
         {
             playerInRange = false;
             transparency = 1.0f;
         }
 
+        // If the object is fading, update the alpha value of the material, otherwise apply the solid material
         if (playerInRange)
         {
-			/*
-            gameObject.GetComponent<Renderer>().material = fadeMaterial;
-            mat = gameObject.GetComponent<Renderer>().material;
-            targetColour.a = transparency;
-            mat.SetColor("_Color", targetColour);
-*/
 			for (int i = 0; i < numberOfRenderers; i++)
 			{
 				attachedRenderers[i].material = fadeMaterial;
@@ -116,18 +121,12 @@ public class AnimationObstacleFade : MonoBehaviour {
         }
         else
         {
-            /*
-			gameObject.GetComponent<Renderer>().material = solidMaterial;
-            mat = gameObject.GetComponent<Renderer>().material;
-			*/
-			for (int i = 0; i < numberOfRenderers; i++)
+            //TODO: currently assigning solidMaterial each frame for every object, could be optimised
+            for (int i = 0; i < numberOfRenderers; i++)
 			{
 				attachedRenderers[i].material = solidMaterial;
 				mat = attachedRenderers[i].material;
 			}
         }
-               
-        
-        //attachedRenderer.sharedMaterial.color = new Color(1.0f, 1.0f, 1.0f, transparency);
     }
 }
