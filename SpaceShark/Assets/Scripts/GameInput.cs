ed559f6b778eace1e74abd167dd8b3aaa5f55acd
Josh Hale <josh_8d@hotmail.co.uk>
2017-01-25 23:52:41 +0000

    Documented GameInput Class
    Commented and removed redundant variables

--- SpaceShark/Assets/Scripts/GameInput.cs
+++ SpaceShark/Assets/Scripts/GameInput.cs
@@ -3,45 +3,45 @@ using System.Collections;
 
 public class GameInput : MonoBehaviour
 {
+    // The accepted directions of a swipe input
     public enum Direction { NW, N, NE, W, E, SW, S, SE };
 
+    // Delegate functions for input
     public delegate void OnTapCallback(Vector3 position);
     public delegate void OnSwipeCallback(Direction direction);
 
+    // Callback events for received input
     public static event OnTapCallback OnTap;
     public static event OnSwipeCallback OnSwipe;
 
-    private enum MouseButtons
-    {
-        Left,
-        Right,
-        Middle,
+    // Available mousebuttons and the total thereof
+    private enum MouseButtons { Left, Right, Middle, NumMouseButtons };
 
-        NumMouseButtons
-    };
-
+    // How many mousebuttons are available
     private const int kNumMouseButtons = (int)MouseButtons.NumMouseButtons;
-
-    private Vector3 mStartPosition;
-    private float mStartTime;
-    private bool[] mMouseButton;
-    private bool[] mMouseButtonLast;
-    private bool mIsPotentiallyTapping;
-    private bool mIsPotentiallySwiping;
-
-    private const float TapMoveThreshold = 50.0f;
-    private const float TapDuration = 0.5f;
-    private const float SwipeDuration = 1.0f;
-
+    // Used for determining if a swipe was straight or diagonal
     private const float DiagonalThresholdLo = 0.462f;
     private const float DiagonalThresholdHi = 0.887f;
 
-    InputData thisInput;
+    [Header("Input Adjustment")]
     [SerializeField]
+    // Used for distinguishing between a swipe or a tap
     private float sensitivity = 200.0f;
+    // Start position of an input
+    private Vector3 mStartPosition;
+    // Start time of an input
+    private float mStartTime;
+    // If the mousebuttons are down
+    private bool[] mMouseButton;
+    // If the mousebuttons wer down last frame
+    private bool[] mMouseButtonLast;
+    // The data received this frame about user input
+    private InputData thisInput;
 
+    // Use this for initialization
     void Start()
     {
+        // Create arrays for available mouse buttons and initialise to false
         mMouseButton = new bool[kNumMouseButtons];
         mMouseButtonLast = new bool[kNumMouseButtons];
         for (int count = 0; count < kNumMouseButtons; count++)
@@ -49,25 +49,26 @@ public class GameInput : MonoBehaviour
             mMouseButton[count] = false;
             mMouseButtonLast[count] = false;
         }
-
+        // Interpret touch inpust as mouse input for calculations
         Input.simulateMouseWithTouches = true;
     }
 
+    // Update is called once per frame
     void Update()
     {
         // Cache the last frame mouse status and read in the current mouse status 
-        for (int count = 0; count < kNumMouseButtons; count++)
+        for (int i = 0; i < kNumMouseButtons; i++)
         {
-            mMouseButtonLast[count] = mMouseButton[count];
-            mMouseButton[count] = Input.GetMouseButton(count);
+            mMouseButtonLast[i] = mMouseButton[i];
+            mMouseButton[i] = Input.GetMouseButton(i);
         }
 
+        // Initialise tap, swipe and direction status
         bool tap = false;
         bool swipe = false;
-
         Direction direction = Direction.W;
 
-        // Detect different input types: Tap and Swipe
+        // If the screen was just touched, create a new input container and store starting information
         if (MouseButtonJustPressed(MouseButtons.Left))
         {
             mStartTime = Time.time;
@@ -77,19 +78,23 @@ public class GameInput : MonoBehaviour
             thisInput.initialPosition = Input.mousePosition;
             thisInput.endPosition = Input.mousePosition;
         }
+        // If the screen is still being touched, record it's latest position and the duration it has been held for
         else if (MouseButtonHeld(MouseButtons.Left))
         {
             thisInput.duration = Time.time - mStartTime;
             thisInput.endPosition = Input.mousePosition;
         }
+        // If the screen is no longer being touched, calculate wether it was a tap or a swipe
         else if (MouseButtonJustReleased(MouseButtons.Left))
         {
             thisInput.endPosition = Input.mousePosition;
             thisInput.CalculateInput();
+            // If the displacement was under the sensitivity threshold, it is a tap
             if (thisInput.speed < (sensitivity - thisInput.displacement))
             {
                 tap = true;
             }
+            // If it was not a tap, calculate the direction of swipe based on vector between start and end point
             else
             {
                 swipe = true;
@@ -98,12 +103,15 @@ public class GameInput : MonoBehaviour
             }
         }
 
+        // If a tap or a swipe was completed this frame, notify Callback Event
         if (tap || swipe)
         {
+            // If it was a tap, send the position of the tap
             if (tap && OnTap != null)
             {
                 OnTap(Input.mousePosition);
             }
+            // If it was a swipe send the direction of the swipe
             else if (swipe && OnSwipe != null)
             {
                 OnSwipe(direction);
@@ -111,6 +119,7 @@ public class GameInput : MonoBehaviour
         }
     }
 
+    // Reset event delegates
     public static void ResetSwipe()
     {
         OnSwipe = null;
@@ -124,6 +133,7 @@ public class GameInput : MonoBehaviour
         }
     }
 
+    // Check if tap already has a delegate assigned
     public static bool CanAddToTap()
     {
         if (OnTap == null)
@@ -132,7 +142,8 @@ public class GameInput : MonoBehaviour
         }
         return false;
     }
-
+    
+    // Calculate the direction of the swipe according to a vector
     private Direction CalculateDirection(Vector3 input)
     {
         Direction swipeDirection = Direction.W;
@@ -187,11 +198,10 @@ public class GameInput : MonoBehaviour
             swipeDirection = Direction.W;
         }
 
-
-
         return swipeDirection;
     }
 
+    // Check the status of the mousebuttons against their cached data from last frame
     private bool MouseButtonJustPressed(MouseButtons button)
     {
         return mMouseButton[(int)button] && !mMouseButtonLast[(int)button];
