cc083d435497496dbc153c523788acc8ef98c0f1
Josh Hale <josh_8d@hotmail.co.uk>
2017-01-25 19:31:04 +0000

    Documented UiManager Script

--- SpaceShark/Assets/Scripts/UiManager.cs
+++ SpaceShark/Assets/Scripts/UiManager.cs
@@ -3,34 +3,32 @@ using System.Collections;
 
 public class UiManager : MonoBehaviour {
 
+    [Header("References To UI Objects")]
 	[SerializeField]
 	private GameObject deathUI;
-
 	[SerializeField]
 	private GameObject victoryUI;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update ()
 	{
 		switch (StateManager.gameState)
 		{
+            // If the player is dead show death UI
 			case StateManager.States.dead:
 				deathUI.SetActive(true);
 				break;
-			case StateManager.States.complete:
+            // If the player has won show complete UI
+            case StateManager.States.complete:
 				victoryUI.SetActive(true);
 				break;
-			default:
-				if (deathUI.active)
+            // If the player has not died or won, show no UI overlay
+            default:
+				if (deathUI.activeInHierarchy)
 				{
 					deathUI.SetActive(false);
 				}
-				if (victoryUI.active)
+				if (victoryUI.activeInHierarchy)
 				{
 					victoryUI.SetActive(false);
 				}
