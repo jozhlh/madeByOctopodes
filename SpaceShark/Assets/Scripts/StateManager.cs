ea3920f7e027daf47dd80fbcfebd849a2f1d8bdb
rizlaking <josh_hale@btinternet.com>
2017-01-27 15:59:13 +0000

    Removed Redundant Code From StateManager
    Obsolete functionality which is in SceneTransition has been removed
    from StateManager

--- SpaceShark/Assets/Scripts/StateManager.cs
+++ SpaceShark/Assets/Scripts/StateManager.cs
@@ -18,25 +18,4 @@ public class StateManager : MonoBehaviour
         // Make sure the state manager persists across all scenes
 		DontDestroyOnLoad(gameObject);
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		switch ((int)gameState)
-		{
-			case 0: // menu
-			break;
-			case 1: // dead
-			break;
-			case 2: // play
-			break;
-		}
-	}
-
-	public void PlayButtonPressed()
-	{
-		gameState = States.menu;
-        // Name of the scene to load
-		SceneManager.LoadScene("prototype_0");
-	}
 }
