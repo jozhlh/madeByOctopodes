425a10003005e7e4806891dea6d41f72cb9d2c21
rizlaking <josh_hale@btinternet.com>
2017-02-17 16:59:21 +0000

    Moved Firing Code To Cannon From LevelManager
    Moved all firing code to Cannon class. Also disabled player firing when
    dead and using the UI

--- SpaceShark/Assets/Scripts/Level/LevelManager.cs
+++ SpaceShark/Assets/Scripts/Level/LevelManager.cs
@@ -12,18 +12,6 @@ public class LevelManager : MonoBehaviour
     // Reference to the player in the scene
     private GameObject player;
 
-    //TODO: Move this to a gun class
-    [SerializeField]
-    // Reference to the bullet prefab which the player fires
-    private GameObject playerBullet;
-    [SerializeField]
-    // The distance from the player's transform to the gun on the model (-1.25, -0.4, 1.5)
-    private Vector3 bulletOffset =  new Vector3(0,0,0);
-    // Holds references to the bullets in the scene
-    private List<GameObject> bulletObjects = new List<GameObject>();
-    // Whether the player is able to shhot
-    private bool playerCanFire = false;
-
     // Initialization
     void Start ()
     {
@@ -39,14 +27,6 @@ public class LevelManager : MonoBehaviour
             segmentDataObjects.Add(segmentData[ob].gameObject);
             segmentData[ob].PlaceSegment();
         }
-
-        // Add callback event for player being able to fire
-        playerCanFire = false;
-        GameInput.ResetTap();
-        if (GameInput.CanAddToTap())
-        {
-            GameInput.OnTap += PlayerFire;
-        }
     }
 	
 	// Update is called once per frame
@@ -58,23 +38,6 @@ public class LevelManager : MonoBehaviour
             seg.GetComponent<SegmentData>().UpdateSegment(player.transform.position.z - (player.transform.localScale.z));
         }
 
-        // When game is in tutorial level restrict player capabilities 
-        if (StateManager.gameState == StateManager.States.tutorial)         
-        {
-            if (Ship_Movement.restrictBullet == true)
-            {
-                GameInput.OnTap -= PlayerFire;
-            }
-            // Re-enable player capabilities 
-            else
-            {
-                if (!playerCanFire)                                         
-                {
-                    GameInput.OnTap += PlayerFire;
-                    playerCanFire = true;
-                }
-            }
-        }
         CleanUp();
     }
 
@@ -111,24 +74,6 @@ public class LevelManager : MonoBehaviour
 
     }
 
-    public void PlayerFire(Vector3 position)
-    {
-        // Fire bullet from left cannon
-        Vector3 bulletStartPos = player.GetComponent<Transform>().position;
-        GameObject bulletLeft = (GameObject)Instantiate(playerBullet, transform.position, transform.rotation);
-        bulletLeft.transform.position = bulletStartPos + bulletOffset;
-        bulletObjects.Add(bulletLeft);
-
-        // Fire bullet from right cannon 
-        GameObject bulletRight = (GameObject)Instantiate(playerBullet, transform.position, transform.rotation);
-        bulletOffset.x = -1 * bulletOffset.x;
-        bulletRight.transform.position = bulletStartPos + bulletOffset;
-        bulletObjects.Add(bulletRight);
-
-        GetComponent<FireTrigger>().ActivateTrigger();
-        //customTriggers.CustomTrigger();
-    }
-
     // Destroy or disable any expired game objects
     public void CleanUp()
     {
@@ -138,23 +83,5 @@ public class LevelManager : MonoBehaviour
         {
             seg.GetComponent<SegmentData>().CleanUpEnemies();
         }
-
-        // Add bullets to clearance list
-        foreach (GameObject bullet in bulletObjects)            
-        {
-            if (bullet.GetComponent<PlayerBullet>().destroyThis)
-            {
-                toBeCleared.Add(bullet);
-            }
-        }
-
-        // Destroy bullets 
-        foreach (GameObject bullet in toBeCleared)                 
-        {
-            bulletObjects.Remove(bullet);
-            bullet.SetActive(false);
-            Destroy(bullet);
-        }
-        toBeCleared.Clear();
     }
 }
---
+++ SpaceShark/Assets/Scripts/PlayerScripts/Cannon.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+
+	[SerializeField]
+    // Reference to the player in the scene
+    private GameObject player;
+	[SerializeField]
+    // Reference to the bullet prefab which the player fires
+    private GameObject playerBullet;
+    [SerializeField]
+    // The distance from the player's transform to the gun on the model (-1.25, -0.4, 1.5)
+    private Vector3 bulletOffset =  new Vector3(0,0,0);
+    // Holds references to the bullets in the scene
+    private List<GameObject> bulletObjects = new List<GameObject>();
+    // Whether the player is able to shhot
+    private bool playerCanFire = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Add callback event for player being able to fire
+        playerCanFire = false;
+        GameInput.ResetTap();
+        if (GameInput.CanAddToTap())
+        {
+            GameInput.OnTap += PlayerFire;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// When game is in tutorial level restrict player capabilities 
+        if (StateManager.gameState == StateManager.States.tutorial)         
+        {
+            if (Ship_Movement.restrictBullet == true)
+            {
+                GameInput.OnTap -= PlayerFire;
+            }
+            // Re-enable player capabilities 
+            else
+            {
+                if (!playerCanFire)                                         
+                {
+                    GameInput.OnTap += PlayerFire;
+                    playerCanFire = true;
+                }
+            }
+        }
+		else if (StateManager.gameState == StateManager.States.dead)
+		{
+			GameInput.ResetTap();
+		}
+        CleanUp();
+	}
+
+	public void PlayerFire(Vector3 position)
+    {
+        // Fire bullet from left cannon
+        Vector3 bulletStartPos = player.GetComponent<Transform>().position;
+        GameObject bulletLeft = (GameObject)Instantiate(playerBullet, transform.position, transform.rotation);
+        bulletLeft.transform.position = bulletStartPos + bulletOffset;
+        bulletObjects.Add(bulletLeft);
+
+        // Fire bullet from right cannon 
+        GameObject bulletRight = (GameObject)Instantiate(playerBullet, transform.position, transform.rotation);
+        bulletOffset.x = -1 * bulletOffset.x;
+        bulletRight.transform.position = bulletStartPos + bulletOffset;
+        bulletObjects.Add(bulletRight);
+
+        GetComponent<FireTrigger>().ActivateTrigger();
+        //customTriggers.CustomTrigger();
+    }
+
+	// Destroy or disable any expired game objects
+    public void CleanUp()
+    {
+        List<GameObject> toBeCleared = new List<GameObject>();
+
+        // Add bullets to clearance list
+        foreach (GameObject bullet in bulletObjects)            
+        {
+            if (bullet.GetComponent<PlayerBullet>().destroyThis)
+            {
+                toBeCleared.Add(bullet);
+            }
+        }
+
+        // Destroy bullets 
+        foreach (GameObject bullet in toBeCleared)                 
+        {
+            bulletObjects.Remove(bullet);
+            bullet.SetActive(false);
+            Destroy(bullet);
+        }
+        toBeCleared.Clear();
+    }
+}
