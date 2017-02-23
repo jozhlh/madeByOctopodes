6057722782628cb7d7be083520fc842f5b56149a
jordancrow7 <Jordan@MacBook-Pro.local>
2017-02-23 16:39:52 +0000

    Added Sound Manager
    Added Sound Manager object and script

---
+++ SpaceShark/Assets/Scripts/Level/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	private uint bankID;
+	[SerializeField]
+	private string soundbankName;
+
+	// Use this for initialization
+	void Start () {
+		// Import Soundbank
+		AkSoundEngine.LoadBank(soundbankName, AkSoundEngine.AK_DEFAULT_POOL_ID,out bankID);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Play Event
+	public void PlayEvent(string eventName, GameObject go)
+	{
+		AkSoundEngine.PostEvent (eventName, go);
+	}
+
+}
