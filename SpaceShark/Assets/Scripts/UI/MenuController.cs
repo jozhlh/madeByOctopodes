662c503176a294076de45d79cd790e8f6630e004
rizlaking <josh_hale@btinternet.com>
2017-04-07 12:54:08 +0100

    Implemented Splash Screen

---
+++ SpaceShark/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuController : MonoBehaviour
+{
+	[SerializeField]
+	private List<GameObject> levels = new List<GameObject>();
+
+	[SerializeField]
+	private float radius;
+	[SerializeField]
+	private bool clock = false;
+	[SerializeField]
+	private bool anti = false;
+
+	private float placementAngle = 0.0f;
+	private Vector3 targetAngle = new Vector3(0.0f, 0.0f, 0.0f);
+	private Vector3 currentAngle = new Vector3(0.0f, 0.0f, 0.0f);
+	[SerializeField]
+	private float rotationSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		// Initialiseinput
+        GameInput.ResetSwipe();
+        GameInput.OnSwipe += HandleOnSwipe;
+		PlacePlanets();
+		currentAngle = transform.eulerAngles;
+	}
+	
+	void PlacePlanets()
+	{
+		//placementAngle = (2 * Mathf.PI) / levels.Count;
+		placementAngle = 360 / levels.Count;
+		int iterator = 0;
+		Quaternion zeroRotation = Quaternion.Euler(0,0,0);
+		foreach (GameObject planet in levels )
+		{
+			planet.transform.Translate(0,0,-radius);
+			planet.transform.RotateAround(transform.position, transform.up, iterator * placementAngle);
+			planet.transform.localRotation = zeroRotation;
+			iterator++;
+		}
+	}
+
+	void RotateClockwise()
+	{
+		targetAngle.y += placementAngle;
+		if (targetAngle.y > 360)
+		{
+			targetAngle.y -= 360;
+		}
+	}
+
+	void RotateAntiClockwise()
+	{
+		targetAngle.y -= placementAngle;
+		if (targetAngle.y < 0)
+		{
+			targetAngle.y += 360;
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (clock)
+		{
+			RotateClockwise();
+			clock = false;
+		}
+		if (anti)
+		{
+			RotateAntiClockwise();
+			anti = false;
+		}
+
+		currentAngle = new Vector3(
+             Mathf.LerpAngle(currentAngle.x, targetAngle.x, rotationSpeed*Time.deltaTime),
+             Mathf.LerpAngle(currentAngle.y, targetAngle.y, rotationSpeed*Time.deltaTime),
+             Mathf.LerpAngle(currentAngle.z, targetAngle.z, rotationSpeed*Time.deltaTime));
+ 
+         transform.eulerAngles = currentAngle;
+		Quaternion zeroRotation = Quaternion.Euler(-currentAngle);
+		 foreach (GameObject planet in levels )
+		{
+			//planet.transform.rotation.eulerAngles = currentAngle;
+			planet.transform.localRotation = zeroRotation;
+		}
+	}
+
+	private void HandleOnSwipe(GameInput.Direction direction)
+    {
+        switch (direction)
+		{
+			case GameInput.Direction.W:
+				//Move player left
+				RotateClockwise();
+				break;
+			case GameInput.Direction.E:
+				//Move player Right
+				RotateAntiClockwise();
+				break;
+			default:
+				break;
+		}
+    }
+}
---
+++ SpaceShark/Assets/Scripts/UI/MenuPlanet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPlanet : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject playButton = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowPlayButton()
+	{
+		playButton.SetActive(true);
+	}
+
+	public void HidePlayButton()
+	{
+		playButton.SetActive(false);
+	}
+}
